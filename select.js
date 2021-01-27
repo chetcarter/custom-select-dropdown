9a55619ed65a62dd0741c2ccc31a2131859486e2
Chet Carter <abazabalv@gmail.com>
2021-01-26 19:06:42 -0700

    Adds li elements

--- select.js
+++ select.js
@@ -8,20 +8,33 @@ export default class Select {
     setupCustomElement(this)
     element.after(this.customElement)
   }
+
+  get selectedOption() {
+    return this.options.find(option => option.selected)
+  }
 }
 
 function setupCustomElement(select) {
   select.customElement.classList.add("custom-select-container")
 
   select.labelElement.classList.add("custom-select-value")
+  select.labelElement.innerText = select.selectedOption.label
   select.customElement.append(select.labelElement)
 
   select.optionsCustomElement.classList.add("custom-select-options")
+  select.options.forEach(option => {
+    const optionElement = document.createElement("li")
+    optionElement.classList.add("custom-select-option")
+    optionElement.classList.toggle("selected", option.selected)
+    optionElement.innerText = option.label
+    optionElement.dataset.value = option.value
+    select.optionsCustomElement.append(optionElement)
+  })
   select.customElement.append(select.optionsCustomElement)
 }
 
 function getFormattedOptions(optionElements) {
-  [...optionElements].map(optionElement => {
+  return [...optionElements].map(optionElement => {
     return {
       value: optionElement.value,
       label: optionElement.label,
